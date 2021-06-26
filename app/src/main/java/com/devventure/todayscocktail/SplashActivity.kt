f7a2ab7bec5cb3a0ac35abfb26bd4aae5e860225
vinibrum <brumc.vini@gmail.com>
2021-06-26 19:53:00 -0300

    Altera o layout da Splash Screen e customiza o tema

--- app/src/main/java/com/devventure/todayscocktail/SplashActivity.kt
+++ app/src/main/java/com/devventure/todayscocktail/SplashActivity.kt
@@ -4,26 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import kotlinx.coroutines.*
+import java.util.concurrent.TimeUnit
 
 class SplashActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-
-
-        //Change to coroutines :D
-        Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
-            finish()
-        },   SPLASH_TIME_OUT )
-    }
-
-    companion object {
-        private const val SPLASH_TIME_OUT : Long = 3000;
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(TimeUnit.SECONDS.toMillis(1))
+            withContext(Dispatchers.Main) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+        }
     }
 }
